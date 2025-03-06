420616c281f60f1baa2506d8f1b69c26f6b3f13b
yanyingwang <yanyingwang@users.noreply.github.com>
2025-03-06 15:16:49 +0000

    Deploy to GitHub pages

---
+++ hj.js
@@ -0,0 +1,40 @@
+console.log("======== Loading highlight jumping ...");
+
+function changeHJ() {
+  oldURL = new URL(event.oldURL);
+  newURL = new URL(event.newURL);
+  oldHash = oldURL.hash.substring(1);
+  newHash = newURL.hash.substring(1);
+  oldHashDecoded = decodeURIComponent(oldHash);
+  newHashDecoded = decodeURIComponent(newHash);
+  console.log(`Hjumping: ${oldHashDecoded} => ${newHashDecoded}`);
+  if (!!oldHashDecoded) {
+    document.getElementsByName(oldHashDecoded)[0].parentElement.style.background = "transparent"
+    if (document.getElementsByName(oldHashDecoded)[0].nextElementSibling) {
+      document.getElementsByName(oldHashDecoded)[0].nextElementSibling.style.background = "transparent"
+    }
+  }
+  if (!!newHashDecoded) {
+    document.getElementsByName(newHashDecoded)[0].parentElement.style.background = "lightyellow"
+    document.getElementsByName(newHashDecoded)[0].nextElementSibling.style.background = "yellow"
+  }
+}
+
+function initHJ () {
+  const urlhashstr = decodeURIComponent(location.hash.substring(1));
+  console.log(`Hjumping: ${urlhashstr}`);
+  if (!!urlhashstr) {
+    document.getElementsByName(urlhashstr)[0].parentElement.style.background = "lightyellow"
+    if (document.getElementsByName(urlhashstr)[0].nextElementSibling) {
+      document.getElementsByName(urlhashstr)[0].nextElementSibling.style.background = "yellow"
+    }
+  }
+}
+
+window.onhashchange = (event) => {
+  changeHJ();
+};
+
+window.onload = (event) => {
+  initHJ();
+};
